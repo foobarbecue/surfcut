9fc4edd7f26fd84086a9e91f1be04b2a07f4e029
Aaron Curtis <foobarbecue@gmail.com>
2023-09-16 05:18:28 -0700

    convert examples to regression tests

--- tests/test_multispeed.py
+++ tests/test_multispeed.py
@@ -1,7 +1,10 @@
-import pytest
 from unittest.mock import patch
 
-@patch("sys.argv",["ffmpeg.py", "-text", "0:0 1:0,speed=3 2:0 3:0"])
-def test_multispeed(regtest):
-    with regtest:
+def test_multispeed_strinp(regtest):
+    with regtest, patch("sys.argv",["ffmpeg.py", "-text", "0:0 1:0,speed=3 2:0 3:0"]):
         import ffmpeg
+
+def test_multispeed_fileinp(regtest, shared_datadir):
+    with patch("sys.argv",["ffmpeg.py", "-file", str(shared_datadir/"trim1.txt")]):
+        with regtest:
+            import ffmpeg
